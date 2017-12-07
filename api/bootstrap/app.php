794b6e420dff9a1ed7ae50efc8f866a8ad144bda
hano <850430486@qq.com>
2017-12-07 18:38:49 +0800

    update

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -60,7 +60,7 @@ $app->singleton(
 */
 
 $app->middleware([
-   App\Http\Middleware\OptionsRequestMiddleware::class
+   App\Http\Middleware\OptionsRequestMiddleware::class,
 ]);
 
 // $app->routeMiddleware([
@@ -79,7 +79,7 @@ $app->middleware([
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 
 /*
