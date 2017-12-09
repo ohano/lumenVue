55e2a107de3bd4a257bb3c6f55c302635e008a7a
hano <499928027@qq.com>
2017-12-09 16:28:37 +0800

    update

---
+++ api/app/Http/Controllers/User/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\User;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+	public function login()
+	{
+		echo 'hano';
+	}
+}
